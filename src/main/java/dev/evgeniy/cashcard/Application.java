80e36ef73c94d5bb9b91d9fe0bafba0c4cabeae1
evgeniyko99 <ekk4357@gmail.com>
2024-04-26 14:09:30 -0400

    Created CashCard, and serialization/deserialization tests

--- src/main/java/dev/evgeniy/cashcard/Application.java
+++ src/main/java/dev/evgeniy/cashcard/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CashCardApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CashCardApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
---
+++ src/main/java/dev/evgeniy/cashcard/models/CashCard.java
@@ -0,0 +1,4 @@
+package dev.evgeniy.cashcard.models;
+
+public record CashCard(Long id, Double amount) {
+}
